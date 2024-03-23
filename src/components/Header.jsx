8ceada5cc8844e51f4afbcaa035df9cdcb0d6861
vaishnav196 <vaish00721@gmail.com>
2024-03-23 19:08:02 +0530

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,22 +5,22 @@ import "bootstrap/dist/js/bootstrap.min.js"
 function Header() {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-info ">
+      <nav className="navbar navbar-expand-lg navbar-light bg-info ms-auto ">
       <div className="container">
-        <Link className="navbar-brand " to="/">DashBoard</Link>
+        <Link className="navbar-brand " to="/"><h3>DashBoard</h3> </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse m-auto" id="navbarNav">
-          <ul className="navbar-nav">
+        <div className="collapse navbar-collapse " id="navbarNav">
+          <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <Link className="nav-link" to="/add-products">Add Products</Link>
+              <Link className="nav-link" to="/products">Products</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link ms-auto" to="/add-products">Add Products</Link>
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/update-products">Update Products</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/products">Products</Link>
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/profile">Profile</Link>
