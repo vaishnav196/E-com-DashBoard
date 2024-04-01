30d0cd8a131713320550b76e8b017e2f41ba0963
vaishnav196 <vaish00721@gmail.com>
2024-04-01 19:09:04 +0530

    .

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,17 +1,40 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 function Products() {
+    const [products, setProducts] = useState([]);
+    const url = "http://localhost:5000";
 
-  const url = "http://localhost:5000";
-   const getProducts=()=>{
-    
-   }
-  
-  return (
-    <div>
-      <h1>Products</h1>
-    </div>
-  )
+    useEffect(() => {
+        getProducts();
+    }, []);
+
+    const getProducts = async () => {
+        try {
+            const response = await axios.get(`${url}/products`);
+
+            if (response.status === 200) {
+                setProducts(response.data);
+            } else {
+                console.error('Failed to fetch products:', response);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+    return (
+        <div>
+            <h1>Products are here</h1>
+            <ul>
+                {products.map(product => (
+                    <li key={product._id}>
+                        <strong>{product.name}</strong> - ${product.price}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
-export default Products
+export default Products;
