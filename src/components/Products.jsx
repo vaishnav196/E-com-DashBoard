5700813463e82c2713d93670a92cdff4d8d63a1f
vaishnav196 <vaish00721@gmail.com>
2024-04-01 18:06:26 +0530

    .

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
 function Products() {
+
+  const url = "http://localhost:5000";
+   const getProducts=()=>{
+    
+   }
+  
   return (
     <div>
       <h1>Products</h1>
