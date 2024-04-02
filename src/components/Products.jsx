7d86ad4257f96272a1e75d57e8edc5f23e73c3d1
vaishnav196 <vaish00721@gmail.com>
2024-04-02 16:30:36 +0530

    .

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -22,9 +22,24 @@ function Products() {
       console.error("Error:", error);
     }
   };
+  
+  const deleteProduct = async (id) => {
+    try {
+      const response = await axios.delete(`${url}/products/${id}`);
+      if (response.status === 200) {
+        console.log("Product deleted successfully");
 
+        getProducts();
+      } else {
+        console.error("Failed to delete product:", response);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
   return (
     <div>
+        <h1 className="text-md-center">Products</h1>
       <h1 className="text-center">Products </h1>
       <table className="table table-dark table-striped container">
         <thead>
@@ -33,32 +48,35 @@ function Products() {
             <th>Name</th>
             <th>Price</th>
             <th>Cateogory</th>
+            <th>operation</th>
           </tr>
         </thead>
         <tbody>
           {products.map((product, i) => (
-            <tr>
-              <td key={i}>{i + 1}</td>
+            <tr key={product._id}>
+              <td >{i + 1}</td>
               <td>{product.name}</td>
               <td>${product.price}</td>
               <td>{product.category}</td>
+              <td><button className="btn btn-danger btn-sm" onClick={()=>{deleteProduct(product._id)}}>delete</button> <button className="btn btn-primary btn-sm">update</button></td>
+              
             </tr>
           ))}
         </tbody>
       </table>
 
-      <div className="container">
+      <div className="container-fluid">
         <div className="row">
          
             {products.map((product, i) => (
-                <div className="col-md-4">
-  <div class="card" key={i} style={{ width: "" }}>
-                <img src="..." class="card-img" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title">{product.name}</h5>
-                  <h5 class="card-title">{product.category}</h5>
-                  <p class="card-text">{product.price}</p>
-                  <button href="" class="btn btn-primary d-block  m-auto w-100">
+                <div className="col-md-4 mb-3"  key={product._id}>
+  <div className="card" style={{ width: "30rem" }}>
+                <img src="..." className="card-img" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">{product.name}</h5>
+                  <h5 className="card-title">{product.category}</h5>
+                  <p className="card-text">{product.price}</p>
+                  <button href="" className="btn btn-primary d-block  m-auto w-100">
                     Add to cart
                   </button>
                 </div>
